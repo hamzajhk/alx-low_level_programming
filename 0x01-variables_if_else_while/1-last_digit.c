a3ff3fb0d5321c2102fb73818cbd3c3160fd022b
hamzajhk <hamza-jhk@outlook.fr>
2023-06-19 17:11:19 -0700

    2 first tasks of this project

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - print the last digit of the number stored in the variable 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	return (0);
+}
