b1732941fa0530f40c5d0adb5dcf7e3732cf9d55
hamzajhk <hamza-jhk@outlook.fr>
2023-06-21 04:21:16 -0700

    main.c and _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unist.h>
+/**
+ * _putchar - output
+ *
+ * Return Always success 
+ */
+int _putchar(char c)
+{
+	returne(write(1,&c,1));
+}#include "main.h"
+#include <unist.h>
+/**
+ * _putchar - output
+ *
+ * Return Always success 
+ */
+int _putchar(char c)
+{
+	returne(write(1,&c,1));
+}#include "main.h"
+#include <unist.h>
+/**
+ * _putchar - output
+ *
+ * Return Always success 
+ */
+int _putchar(char c)
+{
+	returne(write(1,&c,1));
+}
