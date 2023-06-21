6d011a6b0d953ab31c74c59780ca9b2dc94a51e6
hamzajhk <hamza-jhk@outlook.fr>
2023-06-21 15:41:59 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * main - print alphabet lowercase
+ *
+ * Return : Always 0 (success)
+ */
+void print_alphabet(void);
+{
+	char = alphabet
+		for (alphebt = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet"\n");
+}
