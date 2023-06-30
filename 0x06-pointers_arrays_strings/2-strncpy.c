5703b429740cae72bb6bebc62e037771ec896792
hamzajhk <hamza-jhk@outlook.fr>
2023-06-29 18:01:39 -0700

    all the tasks 0 to 7 0x06 c more pointers arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,9 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 	while (j < n && src[j] !='\0')
 	{
 		dest[j] = src [j];
-		j++
+		j++;
 	}
-	while (i < n)
+	while (j < n)
 	{
 		dest[j] = '\0';
 		j++;
